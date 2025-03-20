419b0873af3a00ca73d08d08dd72d8da1cd5a3c0
Esdras Cavalcanti de Almeida <39674011+wobetec@users.noreply.github.com>
2025-03-20 12:22:12 -0300

    first stelv try

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+    <title>Contact me</title>
+</svelte:head>
+<main>
+    <center>
+        <form action="contact">
+            <input type="text" name="name" id="name" placeholder="Name">
+            <input type="email" name="email" id="email" placeholder="Email">
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+            <button type="submit">Send</button>
+        </form>
+    </center>
+</main>
---
+++ src/routes/snake/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>Snake</title>
+</svelte:head>
+<div class="row">
+    <div class="column">
+        <section>
+            <center>
+                <div>
+                    <p class="intro">This is my attempt of snake game to remeber how to use JS.</p>
+                </div>
+                <div id="score-board">
+                    <span class="score-title">Max Score: <span class="score-value" id="max-score">0</span></span>
+                    <span class="score-title">Score: <span class="score-value" id="current-score">0</span></span>
+                </div>
+                <div id="menu">
+                    <button id="start-button">Start</button>
+                </div>
+            </center>
+        </section>
+    </div>
+    <div class="column">
+        <main>
+            <center>
+                <div id="canva">
+                    <canvas id="game-canvas" width="600" height="600"></canvas>
+                </div>
+            </center>
+        </main>
+    </div>
+</div>
+<script src="assets/snake.js"></script>
